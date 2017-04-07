0a41f4738c43edc0f42b76242d775bd27861c32c
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-04-07 11:09:57 +0200

    add /usr/bin to touch command

--- experiment/control.py
+++ experiment/control.py
@@ -47,7 +47,7 @@ source {envscript} &&
 python {cwd}/control.py expand "{folder}" &&
 {binaryLocation} "{folder}/run.yaml" &&
 python {cwd}/control.py analysis "{folder}" &&
-touch "{folder}/success"
+/usr/bin/touch "{folder}/success"
 
 /usr/bin/rm -f {files_to_remove}
     """
